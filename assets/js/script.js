54b2227315d5c41587edb3abb1b316330da764c5
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-09-08 13:18:41 +0000

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const minhaFormacao = document.getElementById('formacao');
+
 const formacao = [{curso: "ADS", tipo: "Faculdade"},
                   {curso: "Pentest", tipo: "Especialidade"},
                   {curso: "Ethical Hacking", tipo: "Especialidade"},
@@ -18,7 +19,23 @@ botaoEnviar.setAttribute('type', 'button');
 botaoEnviar.innerText = 'Enviar';
 insertBotao.appendChild(botaoEnviar);
 
-const form = document.querySelector('form')
-const userNome = document.querySelector('.input-nome');
-const userEmail = document.querySelector('.input-email');
-const mensagem = document.querySelector('.mensagem');
+function salvarFormulario(field){
+  field.preventDefault();
+  const user = document.querySelector('.input-nome').value;
+  const email = document.querySelector('.input-email').value;
+  const msg = document.querySelector('.mensagem').value;
+
+  const visitantes = {
+    Nome: user,
+    Email: email,
+    Mensagem: msg
+  };
+  console.log(visitantes);
+
+  let inputs = document.querySelectorAll('.input-nome, .input-email, .mensagem');
+  inputs.forEach(input => {
+    input.value = "";
+  });
+}
+
+botaoEnviar.addEventListener('click', salvarFormulario);
